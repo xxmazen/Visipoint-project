f85870e3474613964bd40d79dd49967092dc4f63
Mazen <mmazen.mohamed339@gmail.com>
2026-06-09 13:15:00 +0300

    stage

--- src/main/java/Listeners/CustomListeners.java
+++ src/main/java/Listeners/CustomListeners.java
@@ -1,5 +1,4 @@
 package Listeners;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.ITestListener;
--- src/test/java/signupExcution/signUP.java
+++ src/test/java/signupExcution/signUP.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.time.LocalDateTime;
 
 public class signUP {
